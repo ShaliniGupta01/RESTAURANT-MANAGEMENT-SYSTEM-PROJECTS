17b7e496aae0562adb658d48506a73c8376ea55a
Your Name <your.email@example.com>
2025-10-29 02:49:41 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const __dirname = path.dirname(__filename);
 // CORS (allow admin & user apps)
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["https://user-appsg.netlify.app/", "https://restaurant-adminsg.netlify.app/analytics"],
     credentials: true,
   })
 );
