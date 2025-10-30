8facdedab154ba47411dd6034b97499b5fba94f1
Your Name <your.email@example.com>
2025-10-30 13:14:10 +0530

    data saves

--- Frontend/restaurant-admin/src/api/axios.js
+++ Frontend/restaurant-admin/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_BASE_URL || "https://restaurant-backend-1rky.onrender.com/api";
+const API_BASE = process.env.REACT_API_BASE_URL || "https://restaurant-backend-1rky.onrender.com";
 
 const API = axios.create({
   baseURL: API_BASE,
